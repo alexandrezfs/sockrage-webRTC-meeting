e195a04851b787e0c010a3792c15e66c4c97e993
NGUYEN Alexandre <alex.nr@hotmail.fr>
2014-09-20 21:44:30 +0900

    Styled the index page

---
+++ config.js
@@ -0,0 +1,11 @@
+/**
+ * GLOBAL CONFIGURATIONS
+ * @type {{sockrage_addr: string, db: {messages: string}, server_port: number}}
+ */
+exports.configObject = {
+    sockrage_addr : "http://localhost:3000",
+    db : {
+        messages : "sockrage_rtc_messages"
+    },
+    server_port : 3003
+};
---
+++ server.js
@@ -0,0 +1,36 @@
+/**
+ * GLOBAL VARS
+ * @type {exports}
+ */
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = module.exports.app = express();
+var server = require('http').Server(app);
+var config = require('./config');
+
+/**
+ * App settings
+ */
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/**
+ * Rendering Index
+ */
+app.route('/').get(function(req, res) {
+
+    res.render('index', {
+        sockrage_addr : config.configObject.sockrage_addr,
+        db: config.configObject.db
+    });
+
+});
+
+/**
+ * LISTEN SERVER
+ */
+server.listen(config.configObject.server_port, function() {
+    console.log("Express server started on port " + config.configObject.server_port);
+});
